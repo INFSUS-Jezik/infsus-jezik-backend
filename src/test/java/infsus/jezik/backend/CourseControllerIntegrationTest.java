22726441c130ed3c5782a0d05b87559b1b5e2da0
Marko Curkovic <curkovicm4@gmail.com>
2025-05-24 19:44:33 +0200

    backend commit

--- src/test/java/infsus/jezik/backend/CourseControllerIntegrationTest.java
+++ src/test/java/infsus/jezik/backend/CourseControllerIntegrationTest.java
@@ -18,7 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
